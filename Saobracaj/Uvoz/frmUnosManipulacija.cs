d2f87291ee18c002949ec52837bbf69c9d881d9a
PetrovicPantelija <mtasoftver@gmail.com>
2024-02-19 09:15:44 +0100

    24-2-19

--- Saobracaj/Uvoz/frmUnosManipulacija.cs
+++ Saobracaj/Uvoz/frmUnosManipulacija.cs
@@ -203,14 +203,13 @@ namespace Saobracaj.Uvoz
         private void FillDG6Scenario()
         {
            
-            var select = " SELECT VrstaManipulacije.[ID]      ,VrstaManipulacije.[Naziv]    " +
-     " , VrstaManipulacije.[JM]           ,VrstaManipulacije.[JM2] " +
-    " ,VrstaManipulacije.[TipManipulacije]      ,VrstaManipulacije.[OrgJed]      ,[Oznaka]      ,[Relacija],OrganizacioneJedinice.Naziv as OJ " +
-   " ,VrstaManipulacije.[Cena] ,VrstaManipulacije.[Datum] ,VrstaManipulacije.[Korisnik] FROM [VrstaManipulacije] " +
-   " inner join OrganizacioneJedinice on VrstaManipulacije.OrgJed = OrganizacioneJedinice.ID" +
+            var select = " SELECT VrstaManipulacije.[ID]      ,VrstaManipulacije.[Naziv] ,  " +
+ " VrstaManipulacije.[JM] " +
+" ,VrstaManipulacije.[TipManipulacije]      ,VrstaManipulacije.[OrgJed]      ,OrganizacioneJedinice.Naziv as OJ " +
+" ,VrstaManipulacije.[Cena] ,Scenario.Pokret,Scenario.StatusKontejnera,KontejnerStatus.Naziv,  VrstaManipulacije.[Datum] ,VrstaManipulacije.[Korisnik] FROM[VrstaManipulacije] " +
 " inner join Scenario on Scenario.Usluga = VrstaManipulacije.ID " +
 " inner join kontejnerStatus on KontejnerStatus.ID = Scenario.statusKOntejnera " +
-"   where Scenario.ID = " + Convert.ToInt32(cboScenario.SelectedValue) + " order by Scenario.RB asc ";
+"  inner join OrganizacioneJedinice on VrstaManipulacije.OrgJed = OrganizacioneJedinice.ID where Scenario.ID = " + Convert.ToInt32(cboScenario.SelectedValue) + " order by Scenario.RB asc ";
             SqlConnection conn = new SqlConnection(connection);
             var da = new SqlDataAdapter(select, conn);
             var ds = new DataSet();
