ec575b4468f9a6b00ed8f734e56fdc6c4bac82eb
PetrovicPantelija <mtasoftver@gmail.com>
2024-03-25 12:16:29 +0100

    24-03025

--- Saobracaj/Administracija/frmDodeliGrupu.designer.cs
+++ Saobracaj/Administracija/frmDodeliGrupu.designer.cs
@@ -55,7 +55,7 @@ namespace Saobracaj.Administracija
             this.toolStripSeparator1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(795, 31);
+            this.toolStrip1.Size = new System.Drawing.Size(596, 27);
             this.toolStrip1.TabIndex = 146;
             this.toolStrip1.Text = "Osveži";
             // 
@@ -65,7 +65,7 @@ namespace Saobracaj.Administracija
             this.tsNew.Image = ((System.Drawing.Image)(resources.GetObject("tsNew.Image")));
             this.tsNew.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsNew.Name = "tsNew";
-            this.tsNew.Size = new System.Drawing.Size(29, 28);
+            this.tsNew.Size = new System.Drawing.Size(24, 24);
             this.tsNew.Text = "Novi";
             this.tsNew.Click += new System.EventHandler(this.tsNew_Click);
             // 
@@ -75,7 +75,7 @@ namespace Saobracaj.Administracija
             this.tsSave.Image = ((System.Drawing.Image)(resources.GetObject("tsSave.Image")));
             this.tsSave.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsSave.Name = "tsSave";
-            this.tsSave.Size = new System.Drawing.Size(29, 28);
+            this.tsSave.Size = new System.Drawing.Size(24, 24);
             this.tsSave.Text = "tsSave";
             this.tsSave.Click += new System.EventHandler(this.tsSave_Click);
             // 
@@ -85,22 +85,23 @@ namespace Saobracaj.Administracija
             this.tsDelete.Image = ((System.Drawing.Image)(resources.GetObject("tsDelete.Image")));
             this.tsDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsDelete.Name = "tsDelete";
-            this.tsDelete.Size = new System.Drawing.Size(29, 28);
+            this.tsDelete.Size = new System.Drawing.Size(24, 24);
             this.tsDelete.Text = "toolStripButton1";
             this.tsDelete.Click += new System.EventHandler(this.tsDelete_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 43);
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.Size = new System.Drawing.Size(51, 15);
             this.label1.TabIndex = 147;
             this.label1.Text = "Korisnik";
             // 
@@ -108,19 +109,20 @@ namespace Saobracaj.Administracija
             // 
             this.combo_Korisnik.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.combo_Korisnik.FormattingEnabled = true;
-            this.combo_Korisnik.Location = new System.Drawing.Point(20, 63);
-            this.combo_Korisnik.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.combo_Korisnik.Location = new System.Drawing.Point(15, 51);
+            this.combo_Korisnik.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.combo_Korisnik.Name = "combo_Korisnik";
-            this.combo_Korisnik.Size = new System.Drawing.Size(304, 26);
+            this.combo_Korisnik.Size = new System.Drawing.Size(229, 23);
             this.combo_Korisnik.TabIndex = 148;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(357, 43);
+            this.label2.Location = new System.Drawing.Point(268, 35);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 18);
+            this.label2.Size = new System.Drawing.Size(41, 15);
             this.label2.TabIndex = 147;
             this.label2.Text = "Grupa";
             // 
@@ -128,31 +130,31 @@ namespace Saobracaj.Administracija
             // 
             this.combo_Grupa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.combo_Grupa.FormattingEnabled = true;
-            this.combo_Grupa.Location = new System.Drawing.Point(360, 63);
-            this.combo_Grupa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.combo_Grupa.Location = new System.Drawing.Point(270, 51);
+            this.combo_Grupa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.combo_Grupa.Name = "combo_Grupa";
-            this.combo_Grupa.Size = new System.Drawing.Size(263, 26);
+            this.combo_Grupa.Size = new System.Drawing.Size(198, 23);
             this.combo_Grupa.TabIndex = 148;
             // 
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(216)))), ((int)(((byte)(220)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(20, 110);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 89);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(763, 415);
+            this.dataGridView1.Size = new System.Drawing.Size(572, 337);
             this.dataGridView1.TabIndex = 149;
             this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // frmDodeliGrupu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(216)))), ((int)(((byte)(220)))));
-            this.ClientSize = new System.Drawing.Size(795, 537);
+            this.ClientSize = new System.Drawing.Size(596, 436);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.combo_Grupa);
             this.Controls.Add(this.combo_Korisnik);
@@ -160,9 +162,10 @@ namespace Saobracaj.Administracija
             this.Controls.Add(this.label1);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmDodeliGrupu";
             this.Text = "Administracija - Dodela grupa korisniku";
+            this.Load += new System.EventHandler(this.frmDodeliGrupu_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
